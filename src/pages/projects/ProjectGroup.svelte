f9971562b90aa8603aa6eabed80d147e9540dfa8
PhoenixofForce <phoenixofforce@gmail.com>
2023-01-27 20:03:44 +0100

    animated projects site

--- src/pages/projects/ProjectGroup.svelte
+++ src/pages/projects/ProjectGroup.svelte
@@ -1,20 +1,31 @@
 <script>
+  import { fade, slide } from 'svelte/transition';
+  import { flip } from 'svelte/animate';
+
   import Project from './SingleProject.svelte';
   import { projects } from './projectList';
 
   export let projectCountToDisplay = 0;
   export let selectedTags = []
 
+  for(let i = 0; i < projects.length; i++) projects[i].id = i;
+
   if(projectCountToDisplay == 0) projectCountToDisplay = projects.length;
+  let shownProjects = [];
+  filter();
+
+  $: { selectedTags; console.log("test"); filter(); }
+  function filter() {
+    shownProjects = projects.slice(0, projectCountToDisplay);
+    shownProjects = shownProjects.filter(project => selectedTags == 0 || project.tags.some(e => selectedTags.includes(e)));
+  }
 </script>
 
 <div>
   <!-- Listing Projects -->
   <div class="flex">
-    {#each projects as project, i}
-      {#if i < projectCountToDisplay && (selectedTags == 0 || project.tags.some(e => selectedTags.includes(e)))}
-        <div class="project-holder"> <Project { ...project } /> </div>
-      {/if}
+    {#each shownProjects as project (project.id)}
+        <div class="project-holder" transition:fade="{{duration: 250}}" animate:flip="{{duration: 300}}"> <Project { ...project } /> </div>
     {/each}
   </div>
 </div>
